ba61c5d3ba4d424a1c2e1f938702b627e93d7e71
blfuentes <epiphonetodie@gmail.com>
2026-07-13 08:33:05 +0200

    working: 202608

--- FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs
+++ FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs
@@ -1,14 +1,61 @@
 module Puzzle08
 
+open System.Collections.Generic
 open LocalHelper
+open System.Text
+
+let isTest = true
 
 // Part 1
 let SolvePart1 =
-    0
+    let rules = new Dictionary<string,string>()
+    for l in ReadFileAsLines isTest 8 do
+        rules.TryAdd(l[..0], l[1..].Replace(" ", "")) |> ignore
+    
+    let rec generate (stoat: string) (count: int) (goal: int) =
+        if count = goal then
+            stoat.Length
+        else
+            let sb = StringBuilder()
+            stoat |> Seq.map string |> Seq.iter(fun v -> sb.Append(rules[v]) |> ignore)
+            generate (sb.ToString()) (count + 1) goal
+
+    generate "AB" 0 7
+    
 
 // Part 2
 let SolvePart2 =
-    0
+    let rules = new Dictionary<string,string>()
+    for l in ReadFileAsLines isTest 8 do
+        let cleaned = l.Replace(" ", "")
+        let (a, b) = (cleaned[0].ToString(), cleaned[1].ToString())
+        rules.TryAdd(a+b, (a + cleaned[2..] + b)) |> ignore
+        rules.TryAdd(b+a, (b + cleaned[2..] + a)) |> ignore
+    
+    let rec generate (stoat: string) (count: int) (goal: int) =
+        if count = goal then
+            stoat.Length
+        else
+            let sb = StringBuilder()
+            let parts =
+                stoat.ToCharArray() 
+                |> Seq.pairwise 
+                |> Seq.map(fun (a,b) -> System.String [|a; b|])  
+                |> Seq.map (fun r -> rules[r])
+            if parts |> Seq.length > 1 then
+                parts
+                |> Seq.iteri(fun i p -> 
+                    if i = 0 then 
+                        sb.Append (p.Substring(0, p.Length - 1)) |> ignore
+                    else
+                        sb.Append p[1..] |> ignore
+                )
+            else
+                sb.Append (parts |> Seq.head) |> ignore
+            
+            generate (sb.ToString()) (count + 1) goal
+
+    generate "AB" 0 7
 
 // Part 3
 let SolvePart3 =
--- README.md
+++ README.md
@@ -50,7 +50,7 @@ https://flipflop.slome.org/2025/demo
 | [Puzzle 05](https://flipflop.slome.org/2026/5)  [One Way City ~ Connections With The Mayor ~ Illegal Turnings] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle05/Puzzle05.fs) |
 | [Puzzle 06](https://flipflop.slome.org/2026/6)  [Gears And Lights ~ Bluetooth Gears ~ In It's Prime] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle06/Puzzle06.fs) |
 | [Puzzle 07](https://flipflop.slome.org/2026/7)  [Ross's Pet Snake ~ Reminds Me Of A Certain Game ~ Unstoppable Force Meets Immovable Object] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle07/Puzzle07.fs) |
-| [Puzzle 08](https://flipflop.slome.org/2026/8)  [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs) |
+| [Puzzle 08](https://flipflop.slome.org/2026/8)  [The Amazing Digital Stoats ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs) |
 | [Puzzle 09](https://flipflop.slome.org/2026/9)  [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs) |
 | [Puzzle 10](https://flipflop.slome.org/2026/10) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs) |
 | [Puzzle 11](https://flipflop.slome.org/2026/11) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle11/Puzzle11.fs) |
