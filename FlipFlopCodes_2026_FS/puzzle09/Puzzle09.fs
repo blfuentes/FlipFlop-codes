7179512a8d3d99746a1dd2c7237b4b925aa60b10
blfuentes <epiphonetodie@gmail.com>
2026-07-14 09:27:10 +0200

    working: 202609

--- FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs
+++ FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs
@@ -1,14 +1,170 @@
 module Puzzle09
 
 open LocalHelper
+open System.Collections.Generic
+
+let isTest = true
+let mutable (maxRows, maxCols) = (0, 0)
+
+type CellType =
+| Wall
+| Empty
+
+type Cell = {
+    Row: int
+    Col: int
+    Kind: CellType
+}
 
 // Part 1
 let SolvePart1 =
-    0
+    let input = ReadFileAsLines isTest 9
+    maxRows <- input.Length
+    maxCols <- input[0].Length
+    let mutable (startRow, startCol) = (0, 0)
+    let mutable (endRow, endCol) = (0, 0)
+    let maze = Array2D.init<Cell> maxRows maxCols (fun r c ->
+        let t =
+            match input[r][c] with
+            | '#' ->
+                Wall
+            | 'S' ->
+                startRow <- r
+                startCol <- c
+                Empty
+            | '.' ->
+                Empty
+            | 'E' ->
+                endRow <- r
+                endCol <- c
+                Empty
+            | _ -> failwith "invalid cell"
+        { Row = r; Col = c; Kind = t }
+    )
+
+    let getNeighbors (maze: Cell[,]) (x, y) =
+        let rows = maze.GetLength(0)
+        let cols = maze.GetLength(1)
+        [ (x-1, y); (x+1, y); (x, y-1); (x, y+1) ]
+        |> List.filter (fun (r, c) ->
+            r >= 0 && r < rows && c >= 0 && c < cols &&
+            match maze[r, c].Kind with Wall -> false | _ -> true
+        )
+
+    let visited = HashSet<int*int>()
+    let parent = Dictionary<Cell, Cell>()
+    let queue = Queue<Cell>()
+    let start = maze[startRow, startCol]
+    let target = maze[endRow, endCol]
+    queue.Enqueue(start)
+    let mutable found = false
+    while queue.Count > 0 && not found do
+        let current = queue.Dequeue()
+        if current = target then
+            found <- true
+        else
+            for n in getNeighbors maze (current.Row, current.Col) do
+                if not (visited.Contains n) then
+                    visited.Add(n) |> ignore
+                    let parentPos = maze[fst n, snd n]
+                    parent[parentPos] <- current
+                    queue.Enqueue(parentPos)
+    let rec buildPath acc pos =
+        if pos = start then start :: acc
+        elif parent.ContainsKey(pos) then buildPath (pos :: acc) parent[pos]
+        else []
+
+    let path = if found then buildPath [] target else []
+    path.Length - 1 // remove starting point      
 
 // Part 2
 let SolvePart2 =
-    0
+    let input = ReadFileAsLines isTest 9
+    maxRows <- input.Length
+    maxCols <- input[0].Length
+    let mutable (startRow, startCol) = (0, 0)
+    let mutable (endRow, endCol) = (0, 0)
+    let maze = Array2D.init<Cell> maxRows maxCols (fun r c ->
+        let t =
+            match input[r][c] with
+            | '#' ->
+                Wall
+            | 'S' ->
+                startRow <- r
+                startCol <- c
+                Empty
+            | '.' ->
+                Empty
+            | 'E' ->
+                endRow <- r
+                endCol <- c
+                Empty
+            | _ -> failwith "invalid cell"
+        { Row = r; Col = c; Kind = t }
+    )
+
+    let getNeighbors (maze: Cell[,]) (x, y) =
+        let rows = maze.GetLength(0)
+        let cols = maze.GetLength(1)
+        [ (x-1, y); (x+1, y); (x, y-1); (x, y+1) ]
+        |> List.filter (fun (r, c) ->
+            r >= 0 && r < rows && c >= 0 && c < cols &&
+            match maze[r, c].Kind with Wall -> false | _ -> true
+        )
+
+    let visited = HashSet<int*int>()
+    let parent = Dictionary<Cell, Cell>()
+    let queue = Queue<Cell>()
+    let start = maze[startRow, startCol]
+    let target = maze[endRow, endCol]
+    queue.Enqueue(start)
+    let mutable found = false
+    while queue.Count > 0 && not found do
+        let current = queue.Dequeue()
+        if current = target then
+            found <- true
+        else
+            for n in getNeighbors maze (current.Row, current.Col) do
+                if not (visited.Contains n) then
+                    visited.Add(n) |> ignore
+                    let parentPos = maze[fst n, snd n]
+                    parent[parentPos] <- current
+                    queue.Enqueue(parentPos)
+    let rec buildPath acc pos =
+        if pos = start then start :: acc
+        elif parent.ContainsKey(pos) then buildPath (pos :: acc) parent[pos]
+        else []
+
+    let path = (if found then buildPath [] target else []) |> Array.ofList
+
+    let mutable direction = if path[0].Row = path[1].Row then 'h' else 'v'
+    let mutable steps = 1
+    let mutable acc = 0
+    for (from, target) in (path[1..] |> Seq.pairwise) do
+        let newdirection = if from.Row = target.Row then 'h' else 'v'
+        let nextExpected =
+            match (newdirection, from.Col - target.Col, from.Row - target.Row) with
+            | ('h', diff, _) when diff < 0 ->
+                // horizontally to the right
+                maze[from.Row, target.Col + 1]
+            | ('h', diff, _) when diff > 0 ->
+                // horizontally to the left, keep up and down
+                maze[from.Row, target.Col - 1]
+            | ('v', _, diff) when diff < 0 ->
+                // vertically to the bottom, keep left and right
+                maze[target.Row + 1, from.Col]
+            | ('v', _, diff) when diff > 0 ->
+                // vertically to the top, keep left and right
+                maze[target.Row - 1, from.Col]
+
+        acc <- acc + 1
+        if direction <> newdirection then 
+            steps <- steps + (if nextExpected.Kind.IsWall then 1 else acc)
+            acc <- 0
+            
+        direction <- newdirection
+
+    steps
 
 // Part 3
 let SolvePart3 =
--- README.md
+++ README.md
@@ -51,7 +51,7 @@ https://flipflop.slome.org/2025/demo
 | [Puzzle 06](https://flipflop.slome.org/2026/6)  [Gears And Lights ~ Bluetooth Gears ~ In It's Prime] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle06/Puzzle06.fs) |
 | [Puzzle 07](https://flipflop.slome.org/2026/7)  [Ross's Pet Snake ~ Reminds Me Of A Certain Game ~ Unstoppable Force Meets Immovable Object] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle07/Puzzle07.fs) |
 | [Puzzle 08](https://flipflop.slome.org/2026/8)  [The Amazing Digital Stoats ~ The Power Of Stoat Friendship ~ Flash Forward] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs) |
-| [Puzzle 09](https://flipflop.slome.org/2026/9)  [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs) |
+| [Puzzle 09](https://flipflop.slome.org/2026/9)  [Thinking With Mazes ~ Do Not Look At The Operational End Of The Device ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs) |
 | [Puzzle 10](https://flipflop.slome.org/2026/10) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs) |
 | [Puzzle 11](https://flipflop.slome.org/2026/11) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle11/Puzzle11.fs) |
 | [Puzzle 12](https://flipflop.slome.org/2026/12) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle12/Puzzle12.fs) |
