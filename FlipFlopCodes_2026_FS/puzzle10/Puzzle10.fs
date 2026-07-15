780c0fd5b2b5289fcd06be1d32c2b2b4cb569ec0
blfuentes <epiphonetodie@gmail.com>
2026-07-15 10:08:11 +0200

    working: 202610

--- FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs
+++ FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs
@@ -1,9 +1,85 @@
 module Puzzle10
 
 open LocalHelper
+open System.Text.RegularExpressions
+
+type Op =
+| LoadIntoReg
+| CopyFromRegToReg
+| AddReg1Reg2ToReg3
+| SubReg1Reg2ToReg3
+| MulReg1Reg2ToReg3
+| ModRegReg2ToReg3
+| IncValReg
+| DecValReg
+| JumpTo
+| JumpIfRegZeroTo
+| JumpIfRegNotZeroTo
+
+let OpOfInt i =
+    match i with 
+    | 0  -> LoadIntoReg
+    | 1  -> CopyFromRegToReg
+    | 2  -> AddReg1Reg2ToReg3
+    | 3  -> SubReg1Reg2ToReg3
+    | 4  -> MulReg1Reg2ToReg3
+    | 5  -> ModRegReg2ToReg3
+    | 6  -> IncValReg
+    | 7  -> DecValReg
+    | 8  -> JumpTo
+    | 9  -> JumpIfRegZeroTo
+    | 10 -> JumpIfRegNotZeroTo
+    | _ -> failwith "invalid op"
+
+let NumOfNas (s: string) =
+    Regex.Count(s, "na")
+
+let DoOp (label: int) (op: Op) ((arg1, arg2, arg3): (int*int*Option<int>)) (registers: uint16[]) =
+    let newLabel =
+        match op with
+        | LoadIntoReg -> 
+            registers[arg2] <- (uint16)arg1
+            label + 1
+        | CopyFromRegToReg -> 
+            registers[arg1] <- registers[arg2]
+            label + 1
+        | AddReg1Reg2ToReg3 -> 
+            registers[arg3.Value] <- ((registers[arg1] + registers[arg2]) + uint16(65535)) % uint16(65535)
+            label + 1
+        | SubReg1Reg2ToReg3 -> 
+            registers[arg3.Value] <- ((registers[arg1] - registers[arg2]) + uint16(65535)) % uint16(65535)
+            label + 1
+        | MulReg1Reg2ToReg3 -> 
+            registers[arg3.Value] <- ((registers[arg1] * registers[arg2]) + uint16(65535)) % uint16(65535)
+            label + 1
+        | ModRegReg2ToReg3 -> 
+            registers[arg3.Value] <- if registers[arg2] = uint16(0) then uint16(0) else registers[arg1] % registers[arg2]
+            label + 1
+        | IncValReg -> 
+            registers[arg1] <- ((registers[arg1] + (uint16)1) + uint16(65535)) % uint16(65535)
+            label + 1
+        | DecValReg -> 
+            registers[arg1] <- ((registers[arg1] - (uint16)1) + uint16(65535)) % uint16(65535)
+            label + 1
+        | JumpTo -> 
+            arg1
+        | JumpIfRegZeroTo ->
+            if registers[arg1] = (uint16)0 then arg2 else label + 1
+        | JumpIfRegNotZeroTo ->
+            if registers[arg1] <> (uint16)0 then arg2 else label + 1
+    newLabel
+
+let countNas (parts: string array) =
+    let ins = OpOfInt << NumOfNas <| parts[0].Replace("ba", "")
+    let arg1 = NumOfNas <| parts[1]
+    let arg2 = NumOfNas <| parts[2]
+    let arg3 = if parts.Length = 4 then Some(NumOfNas <| parts[3]) else None
+    (ins, arg1, arg2, arg3)
 
 // Part 1
 let SolvePart1 =
+    let instructions = ReadFileAsLines true 10 |> Seq.map _.Split("ne")
+
     0
 
 // Part 2
--- README.md
+++ README.md
@@ -52,6 +52,6 @@ https://flipflop.slome.org/2025/demo
 | [Puzzle 07](https://flipflop.slome.org/2026/7)  [Ross's Pet Snake ~ Reminds Me Of A Certain Game ~ Unstoppable Force Meets Immovable Object] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle07/Puzzle07.fs) |
 | [Puzzle 08](https://flipflop.slome.org/2026/8)  [The Amazing Digital Stoats ~ The Power Of Stoat Friendship ~ Flash Forward] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle08/Puzzle08.fs) |
 | [Puzzle 09](https://flipflop.slome.org/2026/9)  [Thinking With Mazes ~ Do Not Look At The Operational End Of The Device ~ Thinking With Portals] | 🐾 [F#](./FlipFlopCodes_2026_FS/puzzle09/Puzzle09.fs) |
-| [Puzzle 10](https://flipflop.slome.org/2026/10) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs) |
+| [Puzzle 10](https://flipflop.slome.org/2026/10) [The Banena™ Programming Language ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle10/Puzzle10.fs) |
 | [Puzzle 11](https://flipflop.slome.org/2026/11) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle11/Puzzle11.fs) |
 | [Puzzle 12](https://flipflop.slome.org/2026/12) [ ~  ~ ] | ❓ [F#](./FlipFlopCodes_2026_FS/puzzle12/Puzzle12.fs) |
