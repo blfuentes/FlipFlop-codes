3b98614b9e557168205ab135d260003bcd4a6ac2
blfuentes <epiphonetodie@gmail.com>
2026-07-16 13:29:40 +0200

    working: 202611

--- FlipFlopCodes_2026_FS/puzzle11/Puzzle11.fs
+++ FlipFlopCodes_2026_FS/puzzle11/Puzzle11.fs
@@ -13,7 +13,7 @@ type Rule = {
 
 // Part 1
 let SolvePart1 () =
-    let input = ReadFileAsLines true 11 |> Seq.filter ((<>) System.Environment.NewLine)
+    let input = ReadFileAsLines false 11 |> Seq.filter ((<>) "")
     let chunks = input |> Seq.chunkBySize 2 |> Array.ofSeq
     let regexPatternTriple = @"(?:\d{2}|XX)(?: {2})(?:\d{2}|XX)(?: {2})(?:\d{2}|XX)"
     let regexPatternSingle = @"(?:\d{2}|XX)"
@@ -44,27 +44,57 @@ let SolvePart1 () =
             yield (treeIdx, tree)
         }
 
+    let producedEnergy (year: int) (sprouts: Queue<Rule*(int*int)>) (stems: HashSet<int*int>) =
+        let stemsTo = stems |> Array.ofSeq
+        //printfn "=== Year %d ===" year
+        stemsTo
+        |> Array.sumBy(fun (stemX, stemY) ->
+            let heightEnergy = min 10 (abs(stemY - 1))
+            let stemsAbove = (stemsTo |> Array.filter(fun (x, y) -> x = stemX && y < stemY)).Length
+            let energyMultiplier = if stemsAbove > 2 then 0 else (3 - stemsAbove)
+            let energy = heightEnergy * energyMultiplier
+            //printfn "Stem at (%d, %d) produces %d (%d * %d)" stemX stemY energy heightEnergy energyMultiplier
+            energy
+        )
+
     let growTree (tree: Dictionary<int, Rule>) (years: int) =
         let mutable currentYear = 0
-        let growingTree = Queue<Rule>()
-        growingTree.Enqueue(tree[0])
-        while currentYear < years do
-            let newRules = 
-                [while growingTree.Count > 0 do
-                    let rule = growingTree.Dequeue()
-                    if rule.Left.IsSome then
-                        yield tree[rule.Left.Value.From]
-                    if rule.Right.IsSome then
-                        yield tree[rule.Right.Value.From]
-                    if rule.Above.IsSome then
-                        yield tree[rule.Above.Value.From]
+        let sprouts = Queue<Rule*(int*int)>()
+        let stems = HashSet<int*int>()
+        sprouts.Enqueue(tree[0], (0, 0))
+        let mutable enoughEnergy = true
+        while currentYear < years && enoughEnergy do
+            let positions = 
+                [while sprouts.Count > 0 do
+                    let (rule, (x, y)) = sprouts.Dequeue()
+
+                    if rule.Left.IsSome && not (stems.Contains (x-1, y)) then
+                        stems.Add (x, y) |> ignore // convert sprout into stem if generates new sprout                        
+                        yield (tree[rule.Left.Value.From], (x - 1, y))
+                    if rule.Right.IsSome && not (stems.Contains (x+1, y)) then
+                        stems.Add (x, y) |> ignore // convert sprout into stem if generates new sprout
+                        yield (tree[rule.Right.Value.From], (x + 1, y))
+                    if rule.Above.IsSome && not (stems.Contains (x, y - 1)) then
+                        stems.Add (x, y) |> ignore // convert sprout into stem if generates new sprout
+                        yield (tree[rule.Above.Value.From], (x, y - 1))
                 ]
-            newRules |> Seq.iter(fun r -> growingTree.Enqueue(r))
+            positions
+            |> List.groupBy snd
+            |> List.map snd
+            |> List.iter(fun compareRules ->
+                let highestRule = compareRules |> List.maxBy(fun (r, _) -> r.From)
+                sprouts.Enqueue(highestRule)
+            )
+            currentYear <- currentYear + 1       
+            let requiredEnergy = sprouts.Count * 3 + stems.Count * 3
+            let energy = producedEnergy currentYear sprouts stems
+            enoughEnergy <- if currentYear < 5 || energy >= requiredEnergy then true else false
+            //printfn "At year %d produces %d energy and requires %d" currentYear energy requiredEnergy
+        let biologicalMass = (sprouts.Count + stems.Count)
+        printfn "Tree died with %d years and biological of %d" currentYear biologicalMass
+        biologicalMass
 
-            currentYear <- currentYear + 1
-        growingTree.Count
-
-    rulesByTree |> Seq.map (fun (i, t) -> growTree t 5) |> Seq.sum
+    rulesByTree |> Seq.map (fun (i, t) -> growTree t 100) |> Seq.sum
 
 // Part 2
 let SolvePart2 () =
